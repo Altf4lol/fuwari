1ca88891f42a0f3f562a0e7d255e8fa1104f5b35
Altf4lol <86516298+Altf4lol@users.noreply.github.com>
2026-04-19 21:08:55 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "",
+	title: "Yeet",
 	subtitle: "Magust",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
